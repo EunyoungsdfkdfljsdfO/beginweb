15dedd1827e0d0a9521f45522a4d4b747be1d39f
eunyoung <ddkssudd123@gmail.com>
2024-03-02 21:54:14 +0900

    48.

--- react-project/01-starting-project/01-starting-project/src/App.jsx
+++ react-project/01-starting-project/01-starting-project/src/App.jsx
@@ -2,7 +2,7 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
-
+import TabButton from './components/TabButton/TabButton.jsx';
 
 
 function App() {
@@ -18,9 +18,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <p>
-          <h2>Time to get started!</h2>
-        </p>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
