3577b67df66723b27244aca10331530c1ed7e7d3
eunyoung <ddkssudd123@gmail.com>
2024-03-25 22:23:03 +0900

    deconstructuring

--- react-project/dreamcoding/projects/react/basic/src/components/Profile.jsx
+++ react-project/dreamcoding/projects/react/basic/src/components/Profile.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function Profile(props) {
+export default function Profile({image, name, title}) {
     return (
         <div className='profile'>
-            <img className='photo' src={props.image} alt='avatar'/>
-            <h1>{props.name}</h1>
-            <p>{props.title}</p>
+        <img className='photo' src={image} alt='avatar'/>
+            <h1>{name}</h1>
+            <p>{title}</p>
         </div>
     );
 }
