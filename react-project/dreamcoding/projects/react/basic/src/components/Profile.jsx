14b49eaa130a10ab6d167fedecf9d986b71769af
eunyoung <ddkssudd123@gmail.com>
2024-03-25 22:22:11 +0900

    props 사용해보기

--- react-project/dreamcoding/projects/react/basic/src/components/Profile.jsx
+++ react-project/dreamcoding/projects/react/basic/src/components/Profile.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-export default function Profile() {
+export default function Profile(props) {
     return (
         <div className='profile'>
-           <img className='photo' 
-                src='https://images.unsplash.com/photo-1603575448878-868a20723f5d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-                alt='avatar'
-            />
-            <h1>James Kim</h1>
-            <p>프론트엔드 개발자</p>
+            <img className='photo' src={props.image} alt='avatar'/>
+            <h1>{props.name}</h1>
+            <p>{props.title}</p>
         </div>
     );
 }
