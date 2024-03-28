cadacc3193ad99f0c831565468650fd5d3f2b835
eunyoung <ddkssudd123@gmail.com>
2024-03-28 23:35:21 +0900

    avartar 컴포넌트 만들기

--- react-project/dreamcoding/projects/react/basic/src/AppProfile.jsx
+++ react-project/dreamcoding/projects/react/basic/src/AppProfile.jsx
@@ -1,9 +1,13 @@
 import './App.css';
+import Avartar from './components/Avartar';
 import Profile from './components/Profile';
 
 function AppProfile() {
 	return(
 		<>
+      <Avartar
+        image='https://images.unsplash.com/photo-1557862921-37829c790f19?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        isNew={true}/>
       <Profile
         image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
         name='James Kim' 
