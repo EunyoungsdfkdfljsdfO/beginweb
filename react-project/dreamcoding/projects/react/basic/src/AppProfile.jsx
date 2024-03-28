940d502897e04020333ff07bb9c17eb7a33affa3
eunyoung <ddkssudd123@gmail.com>
2024-03-28 23:43:52 +0900

    handling event

--- react-project/dreamcoding/projects/react/basic/src/AppProfile.jsx
+++ react-project/dreamcoding/projects/react/basic/src/AppProfile.jsx
@@ -3,8 +3,12 @@ import Avartar from './components/Avartar';
 import Profile from './components/Profile';
 
 function AppProfile() {
+  const handleClick = (event) => {
+    alert('버튼이 클릭되었습니다!');
+  }
 	return(
 		<>
+    <button onClick={handleClick}>버튼</button>
       <Avartar
         image='https://images.unsplash.com/photo-1557862921-37829c790f19?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         isNew={true}/>
